1a9fd1d7b5ff9dd01e883c17572631c00c1f9d61
neil aular <neilaular01@protonmail.com>
2023-03-05 19:18:15 -0300

    1era actualizacion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Footer from './footer';
+import Footer from './components/Footer';
 import Card from './components/Card'
 
 
